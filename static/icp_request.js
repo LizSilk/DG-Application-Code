c8e3cc3899c9b1403fbcbb3a2f612c2e74430fec
LizSilk <69031131+LizSilk@users.noreply.github.com>
2020-12-16 11:11:51 +1300

    Reformating - using let rather than var

--- static/icp_request.js
+++ static/icp_request.js
@@ -1,20 +1,20 @@
-var iframe = document.getElementById("203407869834061");
+//let iframe = document.getElementById("203407869834061");
 
 iframe.addEventListener("load", function() {
 
-    var icp_input = iframe.contentWindow.document.getElementById("input_264");
+    let icp_input = iframe.contentWindow.document.getElementById("input_264");
 
-    var buttonDiv = iframe.contentWindow.document.getElementById("text_303");
+    let buttonDiv = iframe.contentWindow.document.getElementById("text_303");
     buttonDiv.innerHTML = "<button id=\"lookup-button-icp\" type=\"button\">\n"
         + "Check ICP Number\n" + "</button>"
         + "<div id=\"lookup-output-icp\" data-component = \"text\">" +"</div>";
-    var button = iframe.contentWindow.document.getElementById("lookup-button-icp");
+    let button = iframe.contentWindow.document.getElementById("lookup-button-icp");
 
     iframe.contentWindow.document.getElementById('input_304').value=0;
     iframe.contentWindow.document.getElementById('input_304').dispatchEvent(new Event('change'));
 
     button.addEventListener("click",function (){
-        var form = new FormData();
+        let form = new FormData();
         form.append('ICPNum',icp_input.value.trim())
         Http = new XMLHttpRequest();
         url = 'http://127.0.0.1:5000/icp';
@@ -25,7 +25,7 @@ iframe.addEventListener("load", function() {
             if(Http.status !== 400){
                 iframe.contentWindow.document.getElementById("lookup-output-icp").innerHTML = "<p>" +
                    "The ICP number you have entered is valid!" +"</p>"
-                var json = JSON.parse(Http.responseText)
+                let json = JSON.parse(Http.responseText)
                 iframe.contentWindow.document.getElementById("input_39_addr_line1").value=json.PhysicalAddressNumber +" "+ json.PhysicalAddressStreet
                 iframe.contentWindow.document.getElementById("input_39_addr_line2").value=json.PhysicalAddressSuburb
                 iframe.contentWindow.document.getElementById("input_39_city").value=json.PhysicalAddressTown
