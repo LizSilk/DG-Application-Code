1a674c858ee8fc2546d2d28616879eab5c3f890f
LizSilk <69031131+LizSilk@users.noreply.github.com>
2020-12-16 11:43:08 +1300

    Commented the javascript

--- static/icp_request.js
+++ static/icp_request.js
@@ -1,41 +1,59 @@
-//let iframe = document.getElementById("203407869834061");
+/**
+ * This script looks up the ICP to confirm it is valid
+ * It sends a http request to the python server, and updates the HTML to give the user an answer
+ * This script must be run AFTER icp_request.js as it declares the IFRAME element
+ */
+////the iframe container - not needed as it is declared by send_request.js
+//let IFRAME = document.getElementById("203407869834061");
 
-iframe.addEventListener("load", function() {
+/**
+ * Event lister used so that elements are only accessed after they are loaded
+ */
+IFRAME.addEventListener("load", function() {
+    let icp_input = IFRAME.contentWindow.document.getElementById("input_264");
 
-    let icp_input = iframe.contentWindow.document.getElementById("input_264");
-
-    let buttonDiv = iframe.contentWindow.document.getElementById("text_303");
+    //add button to the HTML
+    let buttonDiv = IFRAME.contentWindow.document.getElementById("text_303");
     buttonDiv.innerHTML = "<button id=\"lookup-button-icp\" type=\"button\">\n"
         + "Check ICP Number\n" + "</button>"
         + "<div id=\"lookup-output-icp\" data-component = \"text\">" +"</div>";
-    let button = iframe.contentWindow.document.getElementById("lookup-button-icp");
+    let button = IFRAME.contentWindow.document.getElementById("lookup-button-icp");
 
-    iframe.contentWindow.document.getElementById('input_304').value=0;
-    iframe.contentWindow.document.getElementById('input_304').dispatchEvent(new Event('change'));
+    //update background field used to hide page break
+    IFRAME.contentWindow.document.getElementById('input_304').value=0;
+    IFRAME.contentWindow.document.getElementById('input_304').dispatchEvent(new Event('change'));
 
+    /**
+     * This function is responsible for sending the ICP number to the python server
+     * When it gets a response it updates the HTML and fills in the address feilds
+     */
     button.addEventListener("click",function (){
+        //send request to python server
         let form = new FormData();
         form.append('ICPNum',icp_input.value.trim())
         Http = new XMLHttpRequest();
         url = 'http://127.0.0.1:5000/icp';
         Http.open("POST", url, true);
         Http.send(form);
+        //wait for response
         Http.onreadystatechange = (e) => {
             console.log(Http.responseText)
+            //if the icp was valid
             if(Http.status !== 400){
-                iframe.contentWindow.document.getElementById("lookup-output-icp").innerHTML = "<p>" +
+                IFRAME.contentWindow.document.getElementById("lookup-output-icp").innerHTML = "<p>" +
                    "The ICP number you have entered is valid!" +"</p>"
                 let json = JSON.parse(Http.responseText)
-                iframe.contentWindow.document.getElementById("input_39_addr_line1").value=json.PhysicalAddressNumber +" "+ json.PhysicalAddressStreet
-                iframe.contentWindow.document.getElementById("input_39_addr_line2").value=json.PhysicalAddressSuburb
-                iframe.contentWindow.document.getElementById("input_39_city").value=json.PhysicalAddressTown
-                iframe.contentWindow.document.getElementById("input_39_state").value=json.PhysicalAddressRegion
-                iframe.contentWindow.document.getElementById("input_39_postal").value=json.PhysicalAddressPostCode
-                iframe.contentWindow.document.getElementById('input_304').value=1;
-                iframe.contentWindow.document.getElementById('input_304').dispatchEvent(new Event('change'));
+                IFRAME.contentWindow.document.getElementById("input_39_addr_line1").value=json.PhysicalAddressNumber +" "+ json.PhysicalAddressStreet
+                IFRAME.contentWindow.document.getElementById("input_39_addr_line2").value=json.PhysicalAddressSuburb
+                IFRAME.contentWindow.document.getElementById("input_39_city").value=json.PhysicalAddressTown
+                IFRAME.contentWindow.document.getElementById("input_39_state").value=json.PhysicalAddressRegion
+                IFRAME.contentWindow.document.getElementById("input_39_postal").value=json.PhysicalAddressPostCode
+                IFRAME.contentWindow.document.getElementById('input_304').value=1;
+                IFRAME.contentWindow.document.getElementById('input_304').dispatchEvent(new Event('change'));
            }
+           //if the ICP was invalid
            else{
-               iframe.contentWindow.document.getElementById("lookup-output-icp").innerHTML = "<p>" +
+               IFRAME.contentWindow.document.getElementById("lookup-output-icp").innerHTML = "<p>" +
                    "The ICP number you have entered is invalid. Please check you have entered it correctly." +"</p>"
             }
         }
--- static/send_request.js
+++ static/send_request.js
@@ -1,50 +1,73 @@
 /**
  * This script looks up the inverter to confirm it is approved
  * It sends a http request to the python server, and updates the HTML to give the user an answer
- * This script must be run before icp_request.js as it declares the iframe element
+ * This script must be run AFTER icp_request.js as it declares the IFRAME element
  */
 
-const iframe = document.getElementById("203407869834061");
+//the iframe container
+const IFRAME = document.getElementById("203407869834061");
 
-iframe.addEventListener("load", function() {
+/**
+ * Event lister used so that elements are only accessed after they are loaded
+ */
+IFRAME.addEventListener("load", function() {
     makeButton(1,"text_279","input_69","input_298")
     makeButton(2,"text_281","input_150","input_305")
     makeButton(3,"text_285","input_160","input_306")
-    makeButton(3,"text_290","input_220","input_307")
+    makeButton(4,"text_290","input_220","input_307")
 
 });
 
+/**
+ * This function is used to create the 'Check Inverter Button' buttons in the HTML
+ * I made it a function as I did not want to have to repeat the code four times
+ * @param num the number of the inverter i.e. is it the first/second/third inverter on the form
+ * @param textID the id of the paragraph element that will be overwritten with the button
+ * @param inputID the ID of the input field for the inverter model
+ * @param backendID the id of the hidden field that lets me hide the page break
+ */
 function makeButton(num, textID, inputID,backendID){
-    const buttonDiv = iframe.contentWindow.document.getElementById(textID);
+    const buttonDiv = IFRAME.contentWindow.document.getElementById(textID);
     buttonDiv.innerHTML = "<button id=\"lookup-button-"+num+"\" type=\"button\">\n"
         + "Check Inverter Model\n" + "</button>"
         + "<div id=\"lookup-output-"+num+"\" data-component = \"text\">" +"</div>";
-    let button1 = iframe.contentWindow.document.getElementById("lookup-button-"+num);
+    let button1 = IFRAME.contentWindow.document.getElementById("lookup-button-"+num);
     button1.addEventListener("click",function(){lookup(inputID,"lookup-output-"+num,backendID)});
-    iframe.contentWindow.document.getElementById(backendID).value=0;
-    iframe.contentWindow.document.getElementById(backendID).dispatchEvent(new Event('change'));
+    IFRAME.contentWindow.document.getElementById(backendID).value=0;
+    IFRAME.contentWindow.document.getElementById(backendID).dispatchEvent(new Event('change'));
 }
 
+/**
+ * this function is responsible for sending the inverter model number to the python server
+ * When it receives a response, it updates the HTML to inform the user
+ * @param inputID the ID of the input field for the inverter model
+ * @param outputID the id of the paragraph the results will go in
+ * @param backendID the id of the hidden field that lets me hide the page break
+ */
 function lookup(inputID,outputID,backendID){
-    let elmnt = iframe.contentWindow.document.getElementById(inputID).value;
+    //send request to python server
+    let elmnt = IFRAME.contentWindow.document.getElementById(inputID).value;
     let form = new FormData();
     form.append('ModelNum',elmnt.trim())
     Http = new XMLHttpRequest();
     url = 'http://127.0.0.1:5000/inverter';
     Http.open("POST", url, true);
     Http.send(form);
+    //wait for response
     Http.onreadystatechange = (e) => {
+        //if the model was not on the list of improved inverters
         if(Http.responseText=="0"){
-            iframe.contentWindow.document.getElementById(backendID).value=0;
-            iframe.contentWindow.document.getElementById(backendID).dispatchEvent(new Event('change'));
-            iframe.contentWindow.document.getElementById(outputID).innerHTML = "<p>" +
+            IFRAME.contentWindow.document.getElementById(backendID).value=0;
+            IFRAME.contentWindow.document.getElementById(backendID).dispatchEvent(new Event('change'));
+            IFRAME.contentWindow.document.getElementById(outputID).innerHTML = "<p>" +
                 "The inverter model you have entered is not on the Clean Energy Council's list of improved inverters.\n Powerco will not accept this application unless the inverter is on this list. It can be found "
                 +"<a href=\"http://www.cleanenergyregulator.gov.au/DocumentAssets/Pages/CEC-approved-inverters.aspx\" target=\"_blank\">here.</a>"+"</p>";
         }
+        //if the model was on the list of improved inverters
         else{
-            iframe.contentWindow.document.getElementById(backendID).value=1;
-            iframe.contentWindow.document.getElementById(backendID).dispatchEvent(new Event('change'));
-            iframe.contentWindow.document.getElementById(outputID).innerHTML = "<p>" +
+            IFRAME.contentWindow.document.getElementById(backendID).value=1;
+            IFRAME.contentWindow.document.getElementById(backendID).dispatchEvent(new Event('change'));
+            IFRAME.contentWindow.document.getElementById(outputID).innerHTML = "<p>" +
                 "The inverter model you have entered is on the Clean Energy Council's list of improved inverters. This list can be found "
                 +"<a href=\"http://www.cleanenergyregulator.gov.au/DocumentAssets/Pages/CEC-approved-inverters.aspx\" target=\"_blank\">here.</a>" + "</p>";
         }
