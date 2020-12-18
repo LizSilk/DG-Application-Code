c829302641b2fad2301260a0e8d37ebcdc611718
LizSilk <69031131+LizSilk@users.noreply.github.com>
2020-12-18 16:24:40 +1300

    formatting

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='DG application code',
     version='1.0',
     long_description=__doc__,
-    packages= find_packages(),
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=['Flask', 'openpyxl']
